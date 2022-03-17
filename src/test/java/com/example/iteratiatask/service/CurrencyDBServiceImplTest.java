7afaf53d32aaa375caf43176caf19f2db6a850f3
Michael Ivanov <eesseth@gmail.com>
2022-03-18 00:21:34 +0300

    Add test for Currency service getById method

--- src/test/java/com/example/iteratiatask/service/CurrencyDBServiceImplTest.java
+++ src/test/java/com/example/iteratiatask/service/CurrencyDBServiceImplTest.java
@@ -1,15 +1,17 @@
 package com.example.iteratiatask.service;
 
 import com.example.iteratiatask.entity.Currency;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CurrencyDBServiceImplTest {
 
     @Autowired
@@ -19,6 +21,14 @@ class CurrencyDBServiceImplTest {
     public void shouldShowList() {
         List<Currency> list = currencyService.getAll();
         assertNotNull(list);
-        System.out.println(list);
+    }
+
+    @Test
+    public void shouldGetCurrencyById() {
+        String id = "R01535";
+        Currency currency = currencyService
+                .getById(id)   // NOK Норвежская крона
+                .orElseThrow(() -> new RuntimeException("Unable to get Currency by id " + id));
+        assertEquals("NOK", currency.getCharCode());
     }
 }
