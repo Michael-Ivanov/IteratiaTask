190ad7bbb50d56b99315f20bc9ed8dd256ef0506
Michael Ivanov <eesseth@gmail.com>
2022-03-17 19:20:54 +0300

    Init commit: start project

---
+++ src/main/java/com/example/iteratiatask/IteratiaTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.iteratiatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IteratiaTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IteratiaTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iteratiatask/IteratiaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iteratiatask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IteratiaTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
