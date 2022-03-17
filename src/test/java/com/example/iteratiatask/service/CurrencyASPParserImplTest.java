2bc771a9bf984560c2c427743273360d7af576ab
Michael Ivanov <eesseth@gmail.com>
2022-03-17 20:46:25 +0300

    Add simple test to check currency parser returns list

---
+++ src/test/java/com/example/iteratiatask/service/CurrencyASPParserImplTest.java
@@ -0,0 +1,28 @@
+package com.example.iteratiatask.service;
+
+import com.example.iteratiatask.entity.Currency;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CurrencyASPParserImplTest {
+
+    @Autowired
+    private CurrencyASPParser parser;
+
+
+    @Test
+    // simple test to check if parser works and returns list
+    public void shouldParseUrlDocument() {
+        List<Currency> list = parser.parseAll();
+        assertFalse(list.isEmpty());
+        assertNotNull(list.get(new Random().nextInt(list.size() - 1)));
+        System.out.println(list);
+    }
+}
