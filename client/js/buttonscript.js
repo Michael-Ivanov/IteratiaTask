079d55bea34eb99da1a55e901cacbc6b9852a125
Michael Ivanov <eesseth@gmail.com>
2022-03-18 22:34:53 +0300

    Add button click function querying for exchange result

--- client/js/buttonscript.js
+++ client/js/buttonscript.js
@@ -1,8 +1,42 @@
 function buttonFunction() {
 
     let val1 = document.getElementById("select1").value;
-    console.log("val1: " + val1);    
     let val2 = document.getElementById("select2").value;
-    console.log("val2: " + val2);
+    let sum = document.getElementById("sum_input").value;
+    operateByCharCodes(val1, val2, parseFloat(sum));
 
+    // get operation object and its result sum
+    function operateByCharCodes(charCode1, charCode2, sum) {
+
+        // introduce graphql query
+        let query = `query getResultOps($charCode1: String, $charCode2: String, $sum: Float) {
+                                operation(charCode1: $charCode1, charCode2: $charCode2, sum: $sum) {
+                                         id,
+                                         date,
+                                         charCode1,
+                                         charCode2,
+                                         exchangeRate,
+                                         sumToExchange,
+                                         resultSum,
+                                }
+                           }`;
+        // fetch json response
+        fetch('http://localhost:8888/graphql', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify({
+                query,
+                variables: {charCode1, charCode2, sum}
+            })
+        })
+            .then((res) => res.json())
+            .then((result) => {
+                // set resultSum value to 'sum_output' input
+                document.getElementById("sum_output").value = result.data.operation.resultSum;
+            });
+        // todo: handle error
+    }
 }
