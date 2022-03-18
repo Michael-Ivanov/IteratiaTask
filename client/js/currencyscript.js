05c33771791c3e187ab1f71f162911543adbe410
Michael Ivanov <eesseth@gmail.com>
2022-03-18 22:31:21 +0300

    Add eventListener to clear output field. Refactor - names and functions

--- client/js/currencyscript.js
+++ client/js/currencyscript.js
@@ -1,17 +1,23 @@
-fetchCurrencies("select1");
-fetchCurrencies("select2");
+populateSelectElement("select1");
+populateSelectElement("select2");
+setHeaderDate();
 
-function fetchCurrencies(id) {
-    var select = document.getElementById(id);
+function populateSelectElement(id) {
+    let select = document.getElementById(id);
+
+    // create and set initial option for 'select'
     select.length = 0;
-
-    var defaultOption = document.createElement('option');
+    let defaultOption = document.createElement('option');
     defaultOption.text = 'Выберите валюту';
-
-
     select.add(defaultOption);
     select.selectedIndex = 0;
 
+    // create listener to clear 'sum_output' field when currency changed
+    select.addEventListener('change', () => {
+        document.getElementById('sum_output').value = '';
+    })
+
+    // fetch currencies list and populate 'select' with list elements
     fetch('http://localhost:8888/graphql', {
         method: "POST",
         headers: {"Content-Type": "application/json"},
@@ -27,13 +33,16 @@ function fetchCurrencies(id) {
                 option.innerText = element.name;
                 select.append(option);
             });
+            // todo: handle error
         });
 }
-
-var dateField = document.getElementById("date");
-let day = new Date(Date.now()).getDate().toString();
-if (day < 10) day = "0" + day;
-let month = new Date(Date.now()).getMonth().toString();
-if (month < 10) month = "0" + month;
-let year = new Date(Date.now()).getFullYear().toString();
-dateField.innerText = day + "." + month + "." + year
+// get current date. format and set date in header
+function setHeaderDate() {
+    let dateField = document.getElementById("date");
+    let day = new Date(Date.now()).getDate().toString();
+    if (day < 10) day = "0" + day;
+    let month = new Date(Date.now()).getMonth().toString();
+    if (month < 10) month = "0" + month;
+    let year = new Date(Date.now()).getFullYear().toString();
+    dateField.innerText = day + "." + month + "." + year
+}
